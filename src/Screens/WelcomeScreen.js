86666c1393ef2f43f21a8a54ea946d59298455fa
musta10 <mustafaenfadal@gmail.com>
2024-10-04 14:23:28 +0100

    pantallalogin

--- src/Screens/WelcomeScreen.js
+++ src/Screens/WelcomeScreen.js
@@ -4,6 +4,7 @@ import {
   Text,
   View,
   Image,
+  TouchableOpacity,
   Button,
   ImageBackground,
 } from "react-native";
@@ -19,11 +20,19 @@ const WelcomeScreen = ({ navigation }) => {
       </View>
       <View style={styles.mainContainer}>
         <Text style={styles.title}>Bienvenido la bestia</Text>
-        <Button title="Inicia sesión" onPress={() => navigation.navigate("Login")} />
-        <Button
-          title="Crear cuenta"
-          onPress={() => navigation.navigate("Register")}
-        />
+        <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Login')}
+      >
+        <Text style={styles.buttonText}>Inicia Sesión</Text>
+      </TouchableOpacity>
+        <TouchableOpacity
+        style={[styles.button, styles.buttonOutline]}
+        onPress={() => navigation.navigate('Register')}
+      >
+        <Text style={[styles.buttonText, styles.buttonOutlineText]}>Crear cuenta</Text>
+      </TouchableOpacity>
+    
       </View>
     </View>
   );
@@ -32,17 +41,42 @@ const WelcomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#f5f7fa',
     position: "relative",
   },
   mainContainer:{
     flex: 1,
     justifyContent: 'center',
-    alignContent: 'center'
+    alignContent: 'center',
+    margin: 15,
   },
-  title: {
-    fontSize: 24,
+ title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#ccc',
+    marginBottom: 50,
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    marginVertical: 15,
+    borderRadius: 30,
     marginBottom: 20,
+  },
+  buttonOutline: {
+    backgroundColor: '#fff',
+    borderColor: '#4CAF50',
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: '#000',
+    fontSize: 18,
+    fontWeight: '600',
+    textAlign: 'center'
+  },
+  buttonOutlineText: {
+    color: '#4CAF50',
   },
   topImageContainer: {},
   topImage: {
