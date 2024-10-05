5e14a74e7738914dbea00145065710d191df9799
musta10 <mustafaenfadal@gmail.com>
2024-10-05 12:40:15 +0100

    authcontext

--- App.js
+++ App.js
@@ -1,30 +1,64 @@
-import * as React from 'react';
+import * as React from "react";
 import { StatusBar } from "expo-status-bar";
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
-import Welcome from './src/Screens/WelcomeScreen';
-import HomeScreen from './src/Screens/HomeScreen';
-import LoginScreen from './src/Screens/LoginScreen';
-import RegisterScreen from './src/Screens/RegisterScreen';
+import React, { useContext } from "react";
+import { AuthProvider, AuthContext } from "./context/AuthContext";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Welcome from "./src/Screens/WelcomeScreen";
+import HomeScreen from "./src/Screens/HomeScreen";
+import LoginScreen from "./src/Screens/LoginScreen";
+import RegisterScreen from "./src/Screens/RegisterScreen";
 
 const Stack = createStackNavigator();
+
+const AppStack = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+
+  return (
+    <Stack.Navigator initialRouteName="Welcome">
+      {isAuthenticated ? (
+        <>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: "Home", headerShown: false }}
+          />
+        </>
+      ) : (
+        <>
+          <Stack.Screen
+            name="Welcome"
+            component={Welcome}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{
+              title: "Bienvenido La Bestia",
+              headerTitleStyle: {
+                fontSize: 24,
+                fontStyle: "bold",
+              },
+            }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={RegisterScreen}
+            options={{ title: "Regístrate" }}
+          />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+};
+
 export default function App() {
   return (
-    <>
-<StatusBar style="auto" />
-<NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
-        <Stack.Screen name="Login" component={LoginScreen}  options={{ title: 'Bienvenido La Bestia', headerTitleStyle: {
-          fontSize: 24,
-          fontStyle: 'bold'
-        } }} />
-        <Stack.Screen name="Register" component={RegisterScreen} options={{ title: 'Regístrate' }} />
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' ,headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
-    </>
+    <AuthProvider>
+      <NavigationContainer>
+        <AppStack />
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
-
